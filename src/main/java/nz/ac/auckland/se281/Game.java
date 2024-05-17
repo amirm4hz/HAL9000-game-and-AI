24d91a24c88291a5ec7944d5c168db7f332cc9ae
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-05-17 17:46:44 +1200

    had some issues with ny logic and had to rearrange how I implemented my code a nd got it to work, also had to change how my code looped through asking for fingers

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -7,14 +7,32 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
 
   private int roundNumber = 1;
+  private String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
-    MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
-    roundNumber++;
+    this.playerName = options[0];
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
+    roundNumber++;
+    int fingers = -1;
+    while (fingers < 0 || fingers > 5) {
+      System.out.println("Give <fingers> and press enter");
+      String input = Utils.scanner.nextLine();
+      try {
+        fingers = Integer.parseInt(input);
+        if (fingers < 0 || fingers > 5) {
+          MessageCli.INVALID_INPUT.printMessage();
+          fingers = -1;
+        }
+      } catch (NumberFormatException e) {
+        MessageCli.INVALID_INPUT.printMessage();
+      }
+    }
+    MessageCli.PRINT_INFO_HAND.printMessage(playerName, Integer.toString(fingers));
+  }
 
   public void endGame() {}
 
