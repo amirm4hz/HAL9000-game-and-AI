6eed09e9139902c0ec7db895556d3dbc94bf01a6
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-05-19 04:18:19 +1200

    finished the java documentation and commented out all of the methods for strategy class and random strategy class

--- src/main/java/nz/ac/auckland/se281/RandomStrategy.java
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -1,7 +1,14 @@
 package nz.ac.auckland.se281;
 
 public class RandomStrategy implements Strategy {
+
+  /**
+   * Returns a random number between 0 and 5.
+   *
+   * @return a random number between 0 and 5
+   */
   public int getFinger() {
+    // return a random number between 0 and 5
     int randomResult = Utils.getRandomNumberRange(0, 5);
     return randomResult;
   }
--- src/main/java/nz/ac/auckland/se281/Strategy.java
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -1,5 +1,11 @@
 package nz.ac.auckland.se281;
 
 public interface Strategy {
+
+  /**
+   * Returns the number of fingers to show in the game.
+   *
+   * @return the number of fingers to show
+   */
   int getFinger();
 }
