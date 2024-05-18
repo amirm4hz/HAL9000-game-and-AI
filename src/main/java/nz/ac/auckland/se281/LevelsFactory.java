1aa685367313e5219d3832ebe94de4b5008d3600
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-05-19 04:11:39 +1200

    java documentation as well as commenting out the method for Levels factory class

--- src/main/java/nz/ac/auckland/se281/LevelsFactory.java
+++ src/main/java/nz/ac/auckland/se281/LevelsFactory.java
@@ -1,13 +1,21 @@
 package nz.ac.auckland.se281;
 
 public class LevelsFactory {
+
+  /**
+   * Creates a new level of the specified type. The level type can be "EASY", "MEDIUM", or "HARD".
+   * If the level type is not recognized, this method returns null.
+   *
+   * @param levelType the type of the level to create
+   * @return the created level, or null if the level type is not recognized
+   */
   public static Levels createLevel(String levelType) {
     switch (levelType.toUpperCase()) {
-      case "EASY":
+      case "EASY": // If the level type is "EASY", create a new EasyLevel object
         return new EasyLevel();
-      case "MEDIUM":
+      case "MEDIUM": // If the level type is "MEDIUM", create a new MediumLevel object
         return new MediumLevel();
-      case "HARD":
+      case "HARD": // If the level type is "HARD", create a new HardLevel object
         return new HardLevel();
       default:
         return null;
