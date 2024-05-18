4728307b45f0db627e596df9c84014e8f2a19140
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-05-19 04:12:56 +1200

    finished java documentation as well as commented out the levels interface

--- src/main/java/nz/ac/auckland/se281/Levels.java
+++ src/main/java/nz/ac/auckland/se281/Levels.java
@@ -1,7 +1,20 @@
 package nz.ac.auckland.se281;
 
 public interface Levels {
+
+  /**
+   * Plays a round of the game.
+   *
+   * @param playerFingers the number of fingers shown by the player
+   * @param oddOrEven the choice of the player (odd or even)
+   * @param playerName the name of the player
+   */
   void play(int playerFingers, String oddOrEven, String playerName);
 
+  /**
+   * Returns the winner of the round.
+   *
+   * @return the winner of the round
+   */
   String getWinner();
 }
